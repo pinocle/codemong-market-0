617ab855c6ace5772de0305f88d290cf45f07bb2
MK <163805322+pinocle@users.noreply.github.com>
2026-07-02 11:58:51 +0900

    Initialize answer code: src/main/java/com/market/common/exception/ResourceNotFoundException.java

---
+++ src/main/java/com/market/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.market.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
